cf32c00b05a6f45ef19cc5277a760331a5e1a2d1
Evgeniy <evgenuel@gmail.com>
2026-01-20 23:26:52 +0300

    migrating to core

--- init.lua
+++ init.lua
@@ -1,28 +1,28 @@
 --[[
     Minetest Random Teleport Mod
-    Version: 1.0.3
+    Version: 1.0.4
     License: GNU Affero General Public License version 3 (AGPLv3)
 ]]--
 
 local players = {}
 
 -- Register rtp privilege
-minetest.register_privilege("rtp", {
+core.register_privilege("rtp", {
     description = "Allows use of random teleportation (/rtp)",
     give_to_singleplayer = false,  -- Give in singleplayer
     give_to_admin = false,         -- Give to admins by default
 })
 
 -- Register the random teleport command "/rtp".
-minetest.register_chatcommand("rtp", {
+core.register_chatcommand("rtp", {
     description = "Random Teleport",
     privs = {
         rtp = true,  -- Requires rtp privilege
     },
     func = function(name)
         -- Privilege check (additional check just in case)
-        if not minetest.check_player_privs(name, {rtp = true}) then
-            minetest.chat_send_player(name, "Error: You don't have permission to use random teleportation!")
+        if not core.check_player_privs(name, {rtp = true}) then
+            core.chat_send_player(name, "Error: You don't have permission to use random teleportation!")
             return false
         end
 
@@ -38,7 +38,7 @@ minetest.register_chatcommand("rtp", {
 
         if teleporting == false then
             table.insert(players, name) -- Insert the player's name into the cooldown timer table.
-            local player = minetest.get_player_by_name(name) -- The player who entered the command.
+            local player = core.get_player_by_name(name) -- The player who entered the command.
 
             if not player then
                 -- Remove player from table if not found
@@ -63,12 +63,12 @@ minetest.register_chatcommand("rtp", {
             -- Move the player to the random position.
             player:set_pos(pos)
 
-            minetest.chat_send_player(name, "Teleporting...")
+            core.chat_send_player(name, "Teleporting...")
 
             -- Wait 3 seconds for the world to generate around the player, then move the player to the surface.
-            minetest.after(3, function()
+            core.after(3, function()
                 -- Re-check if player exists
-                local player = minetest.get_player_by_name(name)
+                local player = core.get_player_by_name(name)
                 if not player then
                     -- Remove player from table if they logged out
                     for k, v in pairs(players) do
@@ -82,7 +82,7 @@ minetest.register_chatcommand("rtp", {
 
                 local player_pos = player:get_pos()
                 if player_pos then
-                    local target_node = minetest.get_node(player_pos)
+                    local target_node = core.get_node(player_pos)
                     if target_node then
                         local node_name = target_node.name -- Get the name of the node at the player's position.
                         if node_name then
@@ -91,7 +91,7 @@ minetest.register_chatcommand("rtp", {
                                 y = y + 1
                                 local pos = vector.new(x, y, z)
                                 player:set_pos(pos)
-                                node_name = minetest.get_node(player:get_pos()).name
+                                node_name = core.get_node(player:get_pos()).name
                                 -- If the player is not at the surface after moving up 500 nodes, return the player to their original position.
                                 if y > 500 then
                                     player:set_pos(old_pos)
@@ -100,9 +100,9 @@ minetest.register_chatcommand("rtp", {
                                 end
                             end
                             if failed == true then
-                                minetest.chat_send_player(name, "Teleportation failed! Returned to " .. minetest.pos_to_string(player:get_pos()) .. ".")
+                                core.chat_send_player(name, "Teleportation failed! Returned to " .. core.pos_to_string(player:get_pos()) .. ".")
                             else
-                                minetest.chat_send_player(name, "Teleported to " .. minetest.pos_to_string(player:get_pos()) .. ".")
+                                core.chat_send_player(name, "Teleported to " .. core.pos_to_string(player:get_pos()) .. ".")
                             end
                         end
                     end
@@ -110,7 +110,7 @@ minetest.register_chatcommand("rtp", {
             end)
 
             -- Remove the player's name from the cooldown timer table after 10 seconds.
-            minetest.after(10, function()
+            core.after(10, function()
                 for k, v in pairs(players) do
                     if v == name then
                         table.remove(players, k)
@@ -120,7 +120,7 @@ minetest.register_chatcommand("rtp", {
             end)
 
         else
-            minetest.chat_send_player(name, "Please wait a few seconds before using /rtp again.")
+            core.chat_send_player(name, "Please wait a few seconds before using /rtp again.")
         end
     end,
 })
